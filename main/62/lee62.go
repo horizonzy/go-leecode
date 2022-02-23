a0e40eb4ccd7bd0a6a7e337009807138b55c4d36
horizonzy <horizonzy@apache.org>
2022-02-24 00:07:03 +0800

    lee 62

---
+++ main/62/lee62.go
@@ -0,0 +1,27 @@
+package main
+
+func main() {
+	i := uniquePaths(3, 2)
+	println(i)
+}
+
+func uniquePaths(m int, n int) int {
+	var dp = make([][]int, m)
+
+	//dp[i][j] = dp[i-1][j] + dp[i][j-1]
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+		dp[i][0] = 1
+	}
+
+	for i := 0; i < n; i++ {
+		dp[0][i] = 1
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+	return dp[m-1][n-1]
+}
