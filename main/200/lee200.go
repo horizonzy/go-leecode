5c074a2b3ee28d8d9c9f1c261a39f8892e458458
horizonzy <horizonzy@apache.org>
2022-03-05 18:12:39 +0800

    optimize 200

--- main/200/lee200.go
+++ main/200/lee200.go
@@ -12,14 +12,13 @@ func main() {
 }
 
 func numIslands(grid [][]byte) int {
-	var m = make(map[string]struct{})
 	len1 := len(grid)
 	len2 := len(grid[0])
 
 	var count int
 	for i := 0; i < len1; i++ {
 		for j := 0; j < len2; j++ {
-			if dsf(grid, i, j, m) {
+			if dsf(grid, i, j) {
 				count++
 			}
 		}
@@ -27,31 +26,20 @@ func numIslands(grid [][]byte) int {
 	return count
 }
 
-func dsf(grid [][]byte, i, j int, m map[string]struct{}) bool {
-	if i < 0 || i > len(grid)-1 || j < 0 || j > len(grid[0])-1 {
+func dsf(grid [][]byte, i, j int) bool {
+	if i < 0 || i > len(grid)-1 || j < 0 || j > len(grid[0])-1 || grid[i][j] == '0' {
 		return false
 	}
-	if grid[i][j] == '0' {
-		return false
-	}
-	key := buildKey(i, j)
-	if _, ok := m[key]; !ok {
-		m[key] = struct{}{}
-		//上
-		dsf(grid, i-1, j, m)
-		//左
-		dsf(grid, i, j-1, m)
-		// 右
-		dsf(grid, i, j+1, m)
-		// 下
-		dsf(grid, i+1, j, m)
+	grid[i][j] = '0'
+	//上
+	dsf(grid, i-1, j)
+	//左
+	dsf(grid, i, j-1)
+	// 右
+	dsf(grid, i, j+1)
+	// 下
+	dsf(grid, i+1, j)
 
-		return true
-	} else {
-		return false
-	}
-}
+	return true
 
-func buildKey(i, j int) string {
-	return fmt.Sprintf("%d,%d", i, j)
 }
